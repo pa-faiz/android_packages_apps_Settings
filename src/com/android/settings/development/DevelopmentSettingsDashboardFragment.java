9b93b8d222e3183f53a9ab0825cfaa8f595e8746
maxwen <max.weninger@gmail.com>
2018-08-20 11:38:23 +0200

    Settings: disable all bugreport settings
    we dont need those
    
    Change-Id: I7dd62da21e7a48b6a8182df60ac1a06461a1fa8e
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    
    Settings: Disable all bugreport settings
    
    we dont need those
    
    Change-Id: I7dd62da21e7a48b6a8182df60ac1a06461a1fa8e

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -616,8 +616,8 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new PreferVonrController(context, lifecycle));
         controllers.add(new MemoryUsagePreferenceController(context));
-        controllers.add(new BugReportPreferenceController(context));
-        controllers.add(new BugReportHandlerPreferenceController(context));
+        //controllers.add(new BugReportPreferenceController(context));
+        //controllers.add(new BugReportHandlerPreferenceController(context));
         controllers.add(new SystemServerHeapDumpPreferenceController(context));
         controllers.add(new DevelopmentMemtagPagePreferenceController(context, fragment));
         controllers.add(new LocalBackupPasswordPreferenceController(context));
@@ -639,7 +639,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new WirelessDebuggingPreferenceController(context, lifecycle));
         controllers.add(new AdbAuthorizationTimeoutPreferenceController(context));
         controllers.add(new LocalTerminalPreferenceController(context));
-        controllers.add(new BugReportInPowerPreferenceController(context));
+        //controllers.add(new BugReportInPowerPreferenceController(context));
         controllers.add(new AutomaticSystemServerHeapDumpPreferenceController(context));
         controllers.add(new MockLocationAppPreferenceController(context, fragment));
         controllers.add(new MockModemPreferenceController(context));
