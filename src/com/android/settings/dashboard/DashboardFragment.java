d424fbe844f501cb77e63b9642c1d791e6b62b8b
Ido Ben-Hur <idoybh2@gmail.com>
2024-01-29 00:58:20 +0530

    Settings: Allow scheduling always on display [2/2]
    * From sunset to sunrise or at a custom time
    * AODSchedule: Add support for mixed time & sun modes [2/2]
    * Also make the code a bit more readable
    
    Change-Id: I3731817d39c6bd6f9910abc8fdaad3d452fa19f7

--- src/com/android/settings/dashboard/DashboardFragment.java
+++ src/com/android/settings/dashboard/DashboardFragment.java
@@ -21,6 +21,9 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.Preference;
 import android.preference.PreferenceManager.OnActivityResultListener;
 import android.text.TextUtils;
 import android.util.ArrayMap;
@@ -76,6 +79,14 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     public static final String CATEGORY = "category";
     private static final String TAG = "DashboardFragment";
     private static final long TIMEOUT_MILLIS = 50L;
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
+
+    static final int MODE_DISABLED = 0;
+    static final int MODE_NIGHT = 1;
+    static final int MODE_TIME = 2;
+    static final int MODE_MIXED_SUNSET = 3;
+    static final int MODE_MIXED_SUNRISE = 4;
+    Preference mAODPref;
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
         "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity"
@@ -180,6 +191,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
         // Set ComparisonCallback so we get better animation when list changes.
         getPreferenceManager().setPreferenceComparisonCallback(
                 new PreferenceManager.SimplePreferenceComparisonCallback());
@@ -258,6 +271,7 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     public void onResume() {
         super.onResume();
         updatePreferenceStates();
+        updateAlwaysOnSummary();
     }
 
     @Override
@@ -714,6 +728,30 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
             latch.await(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
         } catch (InterruptedException e) {
             // Do nothing
+        }
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            default:
+            case MODE_DISABLED:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case MODE_NIGHT:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case MODE_TIME:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+            case MODE_MIXED_SUNSET:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunset);
+                break;
+            case MODE_MIXED_SUNRISE:
+                mAODPref.setSummary(R.string.always_on_display_schedule_mixed_sunrise);
+                break;
         }
     }
 }
